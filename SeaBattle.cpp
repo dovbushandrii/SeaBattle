243653efbb673f786b85a3acea1049e0519c3c6b
dovbushandrii <54791918+dovbushandrii@users.noreply.github.com>
2020-01-07 16:02:42 +0200

    Update SeaBattle.cpp

--- SeaBattle.cpp
+++ SeaBattle.cpp
@@ -1,23 +1,10 @@
-#include <iostream>
-#include <string>
-#include <conio.h>
 #include "SeaBattle.h"
 
 using namespace std;
 
 int main()
 {
-    /*char key = 0;
-    while (key != 13)
-    {
-        key = _getch();
-        cout << (int)key;
-        if (key == 0 || key == -32)
-        {
-            key = _getch();
-            cout << (int)key;
-        }
-    }*/
+
     SeaBattleGame game;
     game.startgame();
 
